b99d180971b6ef53f166208011b5a62ba3c51355
adel <adelelbary11@gmail.com>
2025-05-01 21:08:21 +0300

    Nav Bar

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Typography> Home page</Typography>
+    </Box>
+  );
+};
+export default HomePage;
